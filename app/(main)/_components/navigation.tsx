8c90d2a84b7eac9c68aa3ec357ff80b6bf2c5839
Octavio Acevedo Sanchez <octavio.acevedo.sanchez@gmail.com>
2024-04-09 21:51:51 -0500

    feat(convex schema): add schema tables and functions

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -6,10 +6,13 @@ import { useMediaQuery } from 'usehooks-ts';
 import { usePathname } from 'next/navigation';
 import { cn } from '@/lib/utils';
 import { UserItem } from './user-item';
+import { useQuery } from 'convex/react';
+import { api } from '@/convex/_generated/api';
 
 export const Navigation = (): React.ReactNode => {
 	const pathname = usePathname();
 	const isMobile = useMediaQuery('(max-width: 768px)');
+	const documents = useQuery(api.documents.get);
 
 	const isResizingRef = useRef(false);
 	const sidebarRef = useRef<ElementRef<'aside'>>(null);
@@ -121,7 +124,9 @@ export const Navigation = (): React.ReactNode => {
 					<UserItem />
 				</div>
 				<div className='mt-4 '>
-					<p>Documents</p>
+					{documents?.map(document => (
+						<p key={document._id}>{document.title}</p>
+					))}
 				</div>
 				<div
 					onMouseDown={handleMouseDown}
