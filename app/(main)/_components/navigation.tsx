c5cec8fd4edbd8d3e5e08f1c3fbe71360e120307
Octavio Acevedo Sanchez <octavio.acevedo.sanchez@gmail.com>
2024-04-14 15:38:11 -0500

    feat(sidebar search): add search functionality

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -25,8 +25,10 @@ import {
 	PopoverContent
 } from '@/components/ui/popover';
 import { TrashBox } from './trash-box';
+import { useSearch } from '@/hooks/use-search';
 
 export const Navigation = (): React.ReactNode => {
+	const search = useSearch();
 	const pathname = usePathname();
 	const isMobile = useMediaQuery('(max-width: 768px)');
 	const create = useMutation(api.documents.create);
@@ -149,7 +151,7 @@ export const Navigation = (): React.ReactNode => {
 				</div>
 				<div>
 					<UserItem />
-					<Item label='Search' icon={Search} isSearch onClick={() => {}} />
+					<Item label='Search' icon={Search} isSearch onClick={search.onOpen} />
 					<Item label='Settings' icon={Settings} onClick={() => {}} />
 					<Item onClick={handleCreate} label='New page' icon={PlusCircle} />
 				</div>
